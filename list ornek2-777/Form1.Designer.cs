b8ddbeb951754717c3a6072c9ba47456779d6f91
tugtekinmelis <145451246+tugtekinmelis@users.noreply.github.com>
2023-11-28 12:48:34 +0300

    Add files via upload

---
+++ list ornek2-777/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace list_ornek2_777
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbSayilar = new System.Windows.Forms.ListBox();
+            this.btnBuyuk = new System.Windows.Forms.Button();
+            this.btnKucuk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbSayilar
+            // 
+            this.lbSayilar.FormattingEnabled = true;
+            this.lbSayilar.Location = new System.Drawing.Point(32, 24);
+            this.lbSayilar.Name = "lbSayilar";
+            this.lbSayilar.Size = new System.Drawing.Size(167, 329);
+            this.lbSayilar.TabIndex = 0;
+            // 
+            // btnBuyuk
+            // 
+            this.btnBuyuk.Location = new System.Drawing.Point(223, 65);
+            this.btnBuyuk.Name = "btnBuyuk";
+            this.btnBuyuk.Size = new System.Drawing.Size(159, 61);
+            this.btnBuyuk.TabIndex = 1;
+            this.btnBuyuk.Text = "büyük sayılar";
+            this.btnBuyuk.UseVisualStyleBackColor = true;
+            this.btnBuyuk.Click += new System.EventHandler(this.btnYaz_Click);
+            // 
+            // btnKucuk
+            // 
+            this.btnKucuk.Location = new System.Drawing.Point(223, 190);
+            this.btnKucuk.Name = "btnKucuk";
+            this.btnKucuk.Size = new System.Drawing.Size(159, 61);
+            this.btnKucuk.TabIndex = 2;
+            this.btnKucuk.Text = "küçük sayılar";
+            this.btnKucuk.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 361);
+            this.Controls.Add(this.btnKucuk);
+            this.Controls.Add(this.btnBuyuk);
+            this.Controls.Add(this.lbSayilar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbSayilar;
+        private System.Windows.Forms.Button btnBuyuk;
+        private System.Windows.Forms.Button btnKucuk;
+    }
+}
+
---
+++ list ornek2-777/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace list_ornek2_777
+{
+    public partial class Form1 : Form
+    {
+  
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYaz_Click(object sender, EventArgs e)
+        {
+            int main()
+            {
+
+                int sayi, adet, i;
+                int buyuk;
+
+                printf("Kac Adet Sayi Girilecek? =  ");
+                scanf("%d", &adet);
+
+                //Kullanıcının istediği kadar( adet kadar ) sayı girmesini isteyen döngü        
+                for (i = 0; i < adet; i++)
+                {
+
+                    printf("%d)Sayi Giriniz ", i + 1);  //Kullanıcının kaçıncı sayıyı girdiğini görmek için i+1 olarak gösteriyoruz
+                    scanf("%d", &sayi);
+
+                    if (i == 0)
+                    {                // Döngü ilk döndüğünde yani i=0 da buyuk değişkenini sayiya eşitliyoruz.
+                        buyuk = sayi;        // Bu eşitlemeyi yapmazsak eğer karşılaştıracak buyuk değeri olmayacak.
+                    }
+
+                    //Döngü ilk defa dönmüyorsa eğer!!!
+                    else
+                    {
+
+                        //Buyuk değeri yeni girilen sayidan küçükse en büyük değer o olmadığı için buyuk değerini değiştiriyoruz.
+                        if (buyuk < sayi)
+                        {
+                            buyuk = sayi;
+
+                        }
+    }
+}
